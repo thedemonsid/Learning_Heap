d35e097bf15e5e287718b5be3e8e41d4388747f8
siddhesh <siddheshshrirame@gmail.com>
2023-09-29 18:03:33 +0530

    heap_theory.cpp completing

--- heap/heap_theory.cpp
+++ heap/heap_theory.cpp
@@ -1,13 +1,13 @@
 /*
 ## Heap is special case of binary tree
 >> In which every level is completely filled excpt last level
->> In last level the ondes are inserted left to right
-
-
-
-
-
-
-
-
+>> In last level the nodes are inserted left to right
+>> Heap is of two types :
+  1.max_heap
+  2.min_heap
+>> In max_heap the parent node is always greter then child node and vice versa in min_heap
+>> Heap is used to implem=nt priority queue
+>> heap is implemented using arrays
+>> Heap is also called as [priority queue]
+>> Heap is used to implement [heap sort,graph algorithms,dijkstra's algorithm]
 */
